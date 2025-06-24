fd8c3d2284cd5658ce48ec43d695cbe75a31b13e
Precious Ogwezhi <pogwezhi@gmail.com>
2025-06-24 15:49:12 +0100

    header

--- src/components/HeaderNavBar.jsx
+++ src/components/HeaderNavBar.jsx
@@ -11,7 +11,7 @@ const HeaderNavBar = () => {
         setOpenSideBar((prevState) => !prevState)
     }
   return (
-   <header className="w-screen fixed top-0 left-0  h-20 right-0 px-4 py-4" >
+   <header className="w-screen fixed top-0 left-0  h-17 right-0 px-4 py-4" >
        <div className='flex justify-between items-center w-full md:max-w-[70%] lg:max-w-[60%] md:mx-auto px-4 py-4' >
            <button onClick={toggleSideBar} className={`h-6 w-[14px]  cursor-pointer z-50 ${openSideBar && 'top-0 pt-0 pb-4 mb-3'}`}>{openSideBar ? <FaRegWindowClose size={30}/> : <FaBars size={25}/>}</button>
            <aside className={`sideBar h-screen bg-red-300 text-black fixed top-0 flex flex-col left-0 pt-15 px-10 w-[72] ${openSideBar ? "translate-x-0" : "-translate-x-full"}`}>
