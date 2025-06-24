32540afaf235d623e7ffd2c536fb4273ce10d4d8
Precious Ogwezhi <pogwezhi@gmail.com>
2025-06-24 15:52:31 +0100

    f

--- src/components/FooterNavBar.jsx
+++ src/components/FooterNavBar.jsx
@@ -13,13 +13,13 @@ const FooterNavBar = () => {
     navigate('/calendar');
   };
   return (
-    <footer className=" w-screen fixed bottom-0 left-0 bg-gray-700 z-2 h-20">
+    <footer className=" w-screen fixed bottom-0 left-0 bg-gray-700 z-2 h-17">
         <nav className="flex flex-row items-center justify-between p-4 w-full md:max-w-[70%] lg:max-w-[60%] md:mx-auto">
-           <NavLink to="/"><FaHome size={30}/></NavLink>
-           <button onClick={goToCalendar}><FaCalendarAlt size={30}/></button>
-           <NavLink to="/newTaskForm"><FaPlus size={30} /></NavLink>
-           <a href=""><FaUserLarge size={30} /></a>
-           <a href=""><FaTrashCan size={30}/></a>
+           <NavLink to="/"><FaHome size={25}/></NavLink>
+           <button onClick={goToCalendar}><FaCalendarAlt size={25}/></button>
+           <NavLink to="/newTaskForm"><FaPlus size={25}/></NavLink>
+           <a href=""><FaUserLarge size={25} /></a>
+           <a href=""><FaTrashCan size={25}/></a>
           </nav>
     </footer>
   )
